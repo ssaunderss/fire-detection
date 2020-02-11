3b12c773cd9381e9595a6a7d5d088826172111a3
Sergiu <sergiu@iliev.us>
2020-02-11 16:23:18 -0500

    Created the main function, draft documentation,  grid generation logic

---
+++ main.py
@@ -0,0 +1,41 @@
+'''
+Project: Fire-Detection
+Capabilities: Visualise all the fires in target region of the word using data from historical NASA satellites and mark the most impactful fires.
+MIT License, Copyright (c) 2020, Sergiu Iliev, Austin Saunders, Peng Zeng, Yuan Li
+
+Installation instructions:
+0. Install Python version 3.7x
+1. Place all .py files in the same directory
+2. Download and unzip the data files placing them in a ./data directory
+3. Install required packages (using conda or pip):
+    $ conda install -c plotly plotly=4.5.0
+4. Run main.py
+'''
+import numpy as np
+import pandas as pd
+
+### Define/import all functions
+## Import VIIRS & MODIS
+fires = pd.read_csv('data/fire_nrt_M6_103976.csv') # for debugging we only import the Near-Real-Time MODIS data
+## Generate df from the data
+## Web_scraping function
+
+## Generate Global Grid
+'''
+Generate Global Gid Dataframe which has the holding coordinate pairs for the centers of the cells in a grid with a given resolution
+Inputs and their defaults: s_lat , e_lat = -5 , -45 # start and end lat !they have to be in the same hemisphere i.e. same sign
+                           s_lon , e_lon = 105, 155 # start and end longitude
+                           cell_side_length = 0.75 # kilometers
+'''
+def grid_generator(s_lat= -5, e_lat=-45, s_lon = 105, e_lon = 155, cell_side_length = 0.75):
+  resolution = cell_side_length/(1*100) # degrees of longitude and latitude per kilometer, 1 degree is 100 km at latitude 20 degrees
+
+  lat = np.arange(s_lat, e_lat, np.sign(s_lat)*resolution) # generate latitude vector
+  lon = np.arange(s_lon, e_lon, np.sign(s_lon)*resolution) # generate longitude vector
+  spatial_grid = np.transpose([np.tile(lat, len(lon)), np.repeat(lon, len(lat))]) # merge the two to form coordinate pairs for the center of each cell
+  spatial_grid=pd.DataFrame(spatial_grid) # dataframe holding the centers of each of the grid cells with the given resolution
+  return spatial_grid
+print(grid_generator())
+
+## Call the Visualise function
+generate_map(fires)
--- visualisation.py
+++ visualisation.py
@@ -4,13 +4,16 @@ Required Package: plotly
 $ conda install -c plotly plotly=4.5.0
 
 '''
-
 import pandas as pd
-fires = pd.read_csv('data/fire_nrt_M6_103976.csv')
-
 import plotly.graph_objects as go
-fig = go.Figure(go.Densitymapbox(lat=fires.latitude, lon=fires.longitude, z=fires.brightness,
-                                 radius=1))
-fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+
+def generate_map(fires):
+    fig = go.Figure(go.Densitymapbox(lat=fires.latitude, lon=fires.longitude, z=fires.brightness,
+                                    radius=1))
+    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig.show()
+
+if __name__ == "__main__":
+    fires = pd.read_csv('data/fire_nrt_M6_103976.csv')
+    generate_map(fires)
