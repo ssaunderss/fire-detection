6a1f85354ab95fb94244008d8d33ef8c9fa3d73a
Austin Saunders <pepperzsaunders@gmail.com>
2020-02-13 20:23:03 -0500

    Cleaned Combined DFs
    Cleaned dataframes so they could be combined, groupedby with inplace operations on lat/long for both dfs, put output csv in data file

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import numpy as np
 import pandas as pd
 #import web_scraped_wiki as wsw
 import subprocess
-import plotly.graph_objects
 
 ### Define/import all functions
 ## Import VIIRS & MODIS as df
@@ -43,9 +42,8 @@ Inputs and their defaults: s_lat , e_lat = -5 , -45 # start and end lat !they ha
                            s_lon , e_lon = 105, 155 # start and end longitude
                            cell_side_length = 0.75 # kilometers
 '''
-def grid_generator(s_lat= -5, e_lat=-45, s_lon = 105, e_lon = 155, cell_side_length = 0.75):
+def grid_generator(s_lat= -5, e_lat=-45, s_lon = 105, e_lon = 155, cell_side_length = 0.375):
   resolution = cell_side_length/(1*100) # degrees of longitude and latitude per kilometer, 1 degree is 100 km at latitude 20 degrees
-
   lat = np.arange(s_lat, e_lat, np.sign(s_lat)*resolution) # generate latitude vector
   lon = np.arange(s_lon, e_lon, np.sign(s_lon)*resolution) # generate longitude vector
   spatial_grid = np.transpose([np.tile(lat, len(lon)), np.repeat(lon, len(lat))]) # merge the two to form coordinate pairs for the center of each cell
@@ -53,20 +51,40 @@ def grid_generator(s_lat= -5, e_lat=-45, s_lon = 105, e_lon = 155, cell_side_len
   return spatial_grid
 print(grid_generator())
 
-#Clean VIIRS and MODIS dfs lat/long so that they now correspond to the cells generated in the grid
-viirs_df['lat'] = np.round(viirs_df['latitude'],4)
-viirs_df['long'] = np.round(viirs_df['longitude'],3)
-modis_df['lat'] = np.round(modis_df['latitude'],4)
-modis_df['long'] = np.round(modis_df['longitude'],3)
-
-
 ##TODO Need to figure out how to come up with an efficient way to tie them together
-#Have a one-to-many relationship, so we will assign foreign key values to both of the viirs_df and modis_df
-#what's the most efficient way to create a point to our cell table?
+
+#Cleans lat and long so that they correspond to values generated in our spatial grid by passing
+#columns through a lambda function
+modis_df['latitude'] = modis_df['latitude'].map(lambda x: (((x + 5) // .00375 ) * .00375) - 5)
+modis_df['longitude'] = modis_df['longitude'].map(lambda x: (((x - 105) // .00375) * .00375) + 105)
+viirs_df['latitude'] = viirs_df['latitude'].map(lambda x: (((x + 5) // .00375 ) * .00375) - 5)
+viirs_df['longitude'] = viirs_df['longitude'].map(lambda x: (((x - 105) // .00375) * .00375) + 105)
+
+#Rename of modis columns of dataset so we can concat later
+modis_df.rename(columns={'brightness': 'bright_ti4','bright_t31': 'bright_ti5'}, inplace=True)
+
+#Now that the lat/long correspond, need to groupby lat/long and aggregate the datapoints
+a = modis_df.groupby(['latitude','longitude']).aggregate(lambda x: x.unique().tolist())  #around 20s
+b = viirs_df.groupby(['latitude','longitude']).aggregate(lambda x: x.unique().tolist()) #around 6m51s
+
+##TODO Concat cleaned MODIS and VIIRS together
+dfs = [a, b]
+comb_df = pd.concat(dfs)l
+comb_df.groupby(['latitude','longitude']).agg(sum)  #around 11m27s
+
+#Generate CSV of combined dataframes
+comb_df.to_csv('data/combined_dataframes.csv')
+
+
+
+#tests that there are cells with more than one entry in them
+#a = modis_df.groupby(['latitude','longitude']).aggregate(lambda x: x.unique().tolist())
+#a.loc[np.array(list(map(len,a.brightness.values)))>1]
+
 
 grid = grid_generator()
 grid.rename(columns={0 : 'lat', 1 : 'long'}, inplace=True)
 #grid["primary_key"] = grid[['lat','long']].agg('-'.join, axis=1)
 
 ## Call the Visualise function
-generate_map(modis_df)
+generate_map(comb_df)
