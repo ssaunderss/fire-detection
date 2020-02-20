c02834224712ac7bc0c131e7914472a534c6bd5a
liyn1102 <60233678+liyn1102@users.noreply.github.com>
2020-02-20 11:18:46 -0500

    Update Websource.py

--- Websource.py
+++ Websource.py
@@ -76,32 +76,32 @@ def historical_map():
     #Convert all relevant scraped cells into a DataFrame
     fire_df = pd.DataFrame(Date, columns=["Date"])
     fire_df["States"] = States
-    fire_df["HA"] = HA
+    fire_df["Area_burned"] = HA
     fire_df["Fatalities"] = Fatalities
-    fire_df["Homes"] = Homes
+    fire_df["Homes_damaged"] = Homes
 
     #Need to do some extra cleaning on the dataframe
     fire_df = fire_df.replace(to_replace = "Nil", value = "0")
 
     # cleaning
-    fire_df['HA'] = fire_df['HA'].str.replace(',', '')
+    fire_df['Area_burned'] = fire_df['Area_burned'].str.replace(',', '')
     fire_df['Fatalities'] = fire_df['Fatalities'].str.replace(',', '')
-    fire_df['Homes'] = fire_df['Homes'].str.replace(',', '')
-    fire_df['Homes'] = fire_df['Homes'].str.replace(',', '')
-    fire_df['HA'][7] = 160000
+    fire_df['Homes_damaged'] = fire_df['Homes_damaged'].str.replace(',', '')
+    fire_df['Homes_damaged'] = fire_df['Homes_damaged'].str.replace(',', '')
+    fire_df['Area_burned'][7] = 160000
     fire_df['Fatalities'][4] = 20
-    fire_df['Homes'][19] = 0
+    fire_df['Homes_damaged'][19] = 0
     fire_df['Year'] = fire_df['Date'].str[-4:]
     fire_df['Year'][197] = 2020
 
     # transform data type to numeric
-    fire_df['HA'] = pd.to_numeric(fire_df['HA'], errors='coerce')
+    fire_df['Area_burned'] = pd.to_numeric(fire_df['Area_burned'], errors='coerce')
     fire_df['Fatalities'] = pd.to_numeric(fire_df['Fatalities'], errors='coerce')
-    fire_df['Homes'] = pd.to_numeric(fire_df['Homes'], errors='coerce')
+    fire_df['Homes_damaged'] = pd.to_numeric(fire_df['Homes_damaged'], errors='coerce')
     fire_df['Year'] = pd.to_numeric(fire_df['Year'], errors='coerce')
 
     # pivot table to get summary by state
-    df1=pd.pivot_table(fire_df, index=['States'],values=['HA','Fatalities','Homes'],aggfunc=np.sum)
+    df1=pd.pivot_table(fire_df, index=['States'],values=['Area_burned','Fatalities','Homes_damaged'],aggfunc=np.sum)
     df2=fire_df.groupby('States').Date.nunique()
     wiki_df = pd.concat([df1,df2],axis=1)
     wiki_df= wiki_df.rename(columns={'Date': 'FireCount'})
@@ -109,10 +109,13 @@ def historical_map():
 
     # left join two dataframes
     combine_df = pd.merge(left=city_df,right=wiki_df, how='left', left_on='State', right_on='State_ab')
-
+    # print(combine_df)
+    combine_df.to_csv ('data/websource_combined.csv', header=True)
+   
     # plot on map
-    fig = px.scatter_mapbox(combine_df, lat="Latitude", lon="Longitude", hover_name="City", hover_data=["Fatalities", "Homes"],
-                            color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+    fig = px.scatter_mapbox(combine_df, lat="Latitude", lon="Longitude", hover_name="City", 
+                            hover_data=["Fatalities", "Area_burned", "Homes_damaged", "FireCount"],
+                            zoom=3, height=300)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
